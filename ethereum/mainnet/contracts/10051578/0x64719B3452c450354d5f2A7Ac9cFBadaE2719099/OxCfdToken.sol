a93e1f63135ca385a04ea309234505be706a4f10
0x19 <nevio@eiger.co>
2023-12-28 11:23:17 +0100

    New batch of the contracts vol.25

---
+++ ethereum/mainnet/contracts/10051578/0x64719B3452c450354d5f2A7Ac9cFBadaE2719099/OxCfdToken.sol
@@ -0,0 +1,247 @@
+pragma solidity >0.5.2 <0.6.0;  //0.5.7
+
+interface IERC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function approve(address spender, uint256 value) external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address who) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+library LSafeMath {
+    int256 constant private INT256_MIN = -2**255; 
+    int256 constant private INT256_MAX = 2**255-1; 
+    uint256 constant private uINT256_MAX = 2**255-1; 
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b);
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a);
+
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0);
+        return a % b;
+    }
+
+
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+    function round(uint256 a, uint8 power) internal pure returns (uint256) {
+        if (power<=1){
+            return a;
+        }
+        uint256 tmp = a / (10**uint256(power)) * (10**uint256(power));
+        if (a - tmp >= 10**uint256(power-1) * 5){
+            return tmp+10**uint256(power);
+        }else {
+            return tmp;
+        }
+    }
+    function ceiling (uint256 a, uint8 power) internal pure returns(uint256) {
+        if (power<=1){
+            return a;
+        }
+        uint256 tmp = a / (10**uint256(power)) * (10**uint256(power));
+        if (a - tmp>0){
+            return tmp+10**uint256(power);
+        }else {
+            return tmp;
+        }
+    }
+    function floor (uint256 a, uint8 power) internal pure returns(uint256) {
+        if (power<=1){
+            return a;
+        }
+        uint256 tmp = a / (10**uint256(power)) * (10**uint256(power));
+        return tmp;
+    }
+    
+    function to_int256(uint256 a) internal pure returns (int256) {
+        require(a <= uINT256_MAX);
+        return int256(a);
+    }
+}
+
+
+contract ERC20 is IERC20 {
+    using LSafeMath for uint256;
+
+    mapping (address => uint256) private _balances;
+
+    mapping (address => mapping (address => uint256)) private _allowed;
+
+    uint256 private _totalSupply;
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address owner) public view returns (uint256) {
+        return _balances[owner];
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowed[owner][spender];
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        require(spender != address(0));
+
+        _allowed[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
+        _transfer(from, to, value);
+        emit Approval(from, msg.sender, _allowed[from][msg.sender]);
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        require(spender != address(0));
+
+        _allowed[msg.sender][spender] = _allowed[msg.sender][spender].add(addedValue);
+        emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        require(spender != address(0));
+
+        _allowed[msg.sender][spender] = _allowed[msg.sender][spender].sub(subtractedValue);
+        emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 value) internal {
+        require(to != address(0));
+
+        _balances[from] = _balances[from].sub(value);
+        _balances[to] = _balances[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    function _mint(address account, uint256 value) internal {
+        require(account != address(0));
+
+        _totalSupply = _totalSupply.add(value);
+        _balances[account] = _balances[account].add(value);
+        emit Transfer(address(0), account, value);
+    }
+
+    function _burn(address account, uint256 value) internal {
+        require(account != address(0));
+
+        _totalSupply = _totalSupply.sub(value);
+        _balances[account] = _balances[account].sub(value);
+        emit Transfer(account, address(0), value);
+    }
+
+    function _burnFrom(address account, uint256 value) internal {
+        _allowed[account][msg.sender] = _allowed[account][msg.sender].sub(value);
+        _burn(account, value);
+        emit Approval(account, msg.sender, _allowed[account][msg.sender]);
+    }
+}
+
+contract ERC20Detailed is IERC20 {
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor (string memory name, string memory symbol, uint8 decimals) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+}
+
+contract ERC20Burnable is ERC20 {
+
+    function burn(uint256 value) public {
+        _burn(msg.sender, value);
+    }
+    function burnFrom(address from, uint256 value) public {
+        _burnFrom(from, value);
+    }
+}
+
+contract OxCfdToken is ERC20, ERC20Detailed, ERC20Burnable {
+    constructor(
+        string memory name,     
+        string memory symbol,   
+        uint8 decimals,         
+        uint256 inital_supply   
+    )
+        ERC20Burnable()  
+        ERC20Detailed(name, symbol, decimals)
+        ERC20()
+        public
+    {
+        _mint(msg.sender, inital_supply * (10 ** uint256(decimals)));
+    }
+}
