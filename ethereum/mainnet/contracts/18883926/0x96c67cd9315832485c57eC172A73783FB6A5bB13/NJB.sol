f8334abbe815abe735c209e83d052b886b21b331
0x19 <nevio@eiger.co>
2023-12-28 13:16:02 +0100

    New batch of the contracts vol.27

---
+++ ethereum/mainnet/contracts/18883926/0x96c67cd9315832485c57eC172A73783FB6A5bB13/NJB.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @title: NEIL BURNELL
+/// @author: manifold.xyz
+
+import "./ERC721Creator.sol";
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                         //
+//                                                                                         //
+//               ...cl..,,    'lk:    ';.      'll'    ok.   'c.    ..';;'                 //
+//                   .,:cdo,..  .ol..  .c'        .;;,. co.   :d'    '.;d,                 //
+//                    ':'...'coc'od.;'  ;c     ..    ,clk:   cOl.    .;xl.                 //
+//                            'coxO;'c. .cc.   .'.    .oXl  .xl       ;x'                  //
+//                               'xk';:   ,:;:lod,     .dO:,od'       :x.     ..           //
+//                              ':dKKO:      .';xx'     .lKk'         dx. :l:;'.           //
+//                       .   .'cc;'c0K;      ....':l:.   .Ok.   ..    o0oodl'              //
+//                  ..   .;:cl:....,ONk'   .       ;Ol   .xX;  ':..:. :0d.                 //
+//                    .;:;;;..  ....cKXc  ;l.     .,cx' ..dNl  .clc.  ;k;                  //
+//                  ...      ..',;clx0Xk..oo.     .:'ox.''lNkc:,;;.   cXd.                 //
+//                          ';,,;;;:::kK:,kx.     .c''ko,;cKWk.      c0x'                  //
+//    .   ..       .....,,.        .,:xXd,kx. .    ol ,OK0OKNk'     cKo.         .         //
+//    .....;,'.    .....';,'.'',.    .'O0;dO.'l' .cOc.lKNNK0KXKkollx0x.         .,.  .     //
+//          .,:;'.  ....      .:l;'    oXooKl..';xXo,dNNNx'..';lddl;.           .,'',.     //
+//             ..,,,,'ol........ck,    ;XX0XO,.;O0c,xNNKl.     ..'.      ..      :l;.      //
+//       ...',','.   ,xKOO00OOOkO0c    .oXNNX00XKc:ONXx,...';c:,,.       ,c.    .oO:       //
+//    ..       .;;;:lclOk;,,''',cOKkc:;..kNNNNNOldXNNKxxO0KOo;.  ..     ,dl.  .;c;'.'.     //
+//    ..  ....,'.'::. .c,       .'dKNNKkd0NNNNNKKNNNNNXxc,'..  .cc.     lOl:clllc'''.      //
+//           ..         ..      ';cxKNNNNNNNNNNNNNNNKo,.  .;. 'xo.   .;oOx;..,;.           //
+//                                'lddod0NNNNNNNNXOc..'.  coldk0Oo;;lxl;,'.   .,.          //
+//                   .                  'kNNNNNNNx.   ';ckKKd'..'coll'                     //
+//                  .:'                  cXNNNNNNd. .;kXN0:.                               //
+//              ..   :d;..,ldxkl.        oNNNNNNNKkkKNNNO,.'.                              //
+//               ''',,lko::,.'cxOo,.    ;0NNNNNNNNNNN0oo:..'.                              //
+//                  ....       .xNXOdl,'xNNNNNNNNNXkc.                                     //
+//                              .,:oKNXKXNNNNNNNNXo.                                       //
+//                                  'xXNNNNNNNNNNk.                                        //
+//                                    c0NNNNNNNNNx.                                        //
+//                                     'kNNNNNNNNx.                                        //
+//                                      lNNNNNNNNx.                       neil burnell.    //
+//                                                                                         //
+//                                                                                         //
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+contract NJB is ERC721Creator {
+    constructor() ERC721Creator("NEIL BURNELL", "NJB") {}
+}
