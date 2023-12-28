d3f5bcda058bdbd863a2b6e78ceb420afd2cbfb1
0x19 <nevio@eiger.co>
2023-12-28 10:50:18 +0100

    New batch of the contracts vol.21

---
+++ ethereum/mainnet/contracts/10007279/0xfe7168C1c0d1f839a31A50bAe7A62F96dDd23996/RCP.sol
@@ -0,0 +1,96 @@
+pragma solidity >= 0.4.24 < 0.6.0;
+
+/**
+ * @title RCP Token
+ * @author J. Kwon
+ */
+
+/**
+ * @title ERC20 Standard Interface
+ */
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address who) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title DAU implementation
+ */
+contract RCP is IERC20 {
+    string public name = "Rolling Camp";
+    string public symbol = "RCP";
+    uint8 public decimals = 18;
+    
+    uint256 companyAmount;
+
+    uint256 _totalSupply;
+    mapping(address => uint256) balances;
+
+    // Addresses
+    address public owner;
+    address public company;
+
+    modifier isOwner {
+        require(owner == msg.sender);
+        _;
+    }
+    
+    constructor() public {
+        owner = msg.sender;
+
+        company = 0xA3D4CFa33D792A48295e50C5BF011d2E83E75E34;
+
+        companyAmount  = toWei(200000000);
+        _totalSupply   = toWei(200000000);  //200,000,000
+
+        require(_totalSupply == companyAmount );
+        
+        balances[owner] = _totalSupply;
+
+        emit Transfer(address(0), owner, balances[owner]);
+        
+        transfer(company, companyAmount);
+
+        require(balances[owner] == 0);
+    }
+    
+    function totalSupply() public view returns (uint) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address who) public view returns (uint256) {
+        return balances[who];
+    }
+    
+    function transfer(address to, uint256 value) public returns (bool success) {
+        require(msg.sender != to);
+        require(to != owner);
+        require(value > 0);
+        
+        require( balances[msg.sender] >= value );
+        require( balances[to] + value >= balances[to] );    // prevent overflow
+
+
+        balances[msg.sender] -= value;
+        balances[to] += value;
+
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+    
+    function burnCoins(uint256 value) public {
+        require(balances[msg.sender] >= value);
+        require(_totalSupply >= value);
+        
+        balances[msg.sender] -= value;
+        _totalSupply -= value;
+
+        emit Transfer(msg.sender, address(0), value);
+    }
+
+    function toWei(uint256 value) private view returns (uint256) {
+        return value * (10 ** uint256(decimals));
+    }
+}
