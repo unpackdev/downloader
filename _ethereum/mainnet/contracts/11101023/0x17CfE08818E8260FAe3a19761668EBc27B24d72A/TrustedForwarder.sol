ce280a31cd3b3b46bc8e74f50a94b78a5b775070
0x19 <nevio@eiger.co>
2023-12-31 10:19:12 +0100

    New batch of the contracts vol.40

---
+++ _ethereum/mainnet/contracts/11101023/0x17CfE08818E8260FAe3a19761668EBc27B24d72A/TrustedForwarder.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.6.0 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "./Forwarder.sol";
+
+contract TrustedForwarder is Forwarder {}
--- _ethereum/mainnet/contracts/18000884/0x3518006399eadb30d99fE1530168E5115247004D/IAdapter.sol
+++ _ethereum/mainnet/contracts/18000884/0x3518006399eadb30d99fE1530168E5115247004D/IAdapter.sol
@@ -1,41 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Holdings, 2022
-pragma solidity ^0.8.10;
+// (c) Gearbox Holdings, 2023
+pragma solidity ^0.8.17;
 
-import "./AdapterType.sol";
-
-import "./IACL.sol";
-import "./IAddressProvider.sol";
-import "./ICreditManagerV2.sol";
-
-interface IAdapterExceptions {
-    /// @notice Thrown when adapter tries to use a token that's not a collateral token of the connected Credit Manager
-    error TokenNotAllowedException();
-
-    /// @notice Thrown when caller of a `creditFacadeOnly` function is not the Credit Facade
-    error CreditFacadeOnlyException();
-
-    /// @notice Thrown when caller of a `configuratorOnly` function is not configurator
-    error CallerNotConfiguratorException();
-}
-
-interface IAdapter is IAdapterExceptions {
-    /// @notice Credit Manager the adapter is connected to
-    function creditManager() external view returns (ICreditManagerV2);
-
-    /// @notice Address of the contract the adapter is interacting with
-    function targetContract() external view returns (address);
-
-    /// @notice Address provider
-    function addressProvider() external view returns (IAddressProvider);
-
-    /// @notice ACL contract to check rights
-    function _acl() external view returns (IACL);
-
-    /// @notice Adapter type
-    function _gearboxAdapterType() external pure returns (AdapterType);
-
-    /// @notice Adapter version
-    function _gearboxAdapterVersion() external pure returns (uint16);
-}
+import "./IAdapter.sol";
